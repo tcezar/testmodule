309335f6ee7b5dfa47ba196a4b4b26f1ae3d5db1
tcezar <tcezar@tcezar.ru>
2017-10-15 20:09:42 +0300

    add много плагинов для изучения

---
+++ src/test/java/ru/tcezar/testmodule/test/SimpleTest.java
@@ -0,0 +1,14 @@
+package ru.tcezar.testmodule.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by nat on 15.10.17.
+ * for student test
+ */
+class SimpleTest {
+    @Test
+    void test1(){
+        System.out.println("succes");
+    }
+}
